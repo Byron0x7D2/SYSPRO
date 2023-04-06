fa0da5b2a4c0ee68fd20e83eef153d792522b704
byron <bg.anemogiannis@outlook.com>
2023-04-06 19:47:31 +0300

    d

--- ASS1/include/parsing.h
+++ ASS1/include/parsing.h
@@ -1,7 +1,3 @@
 #pragma once
 
-void get_memory(char ***, char **, char **, char **);
-void free_memory(char **, char *, char *, char *);
-void parse_string(char* , char* , char **);
-// int get_input_token(char *word);
 int command();
