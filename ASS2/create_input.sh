a442f31e0526f21475f07b0564271724ad940272
byron <bg.anemogiannis@outlook.com>
2023-05-17 23:16:23 +0300

    better

--- ASS2/create_input.sh
+++ ASS2/create_input.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+characters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+length=$(( RANDOM % 10 + 3 ))
 
 if [ $# -ne 2 ]; then
 	echo "Usage: $0 <political Parties> <numLines>"
@@ -9,8 +11,14 @@ fi
 touch inputFile
 
 for (( i=0; i<$2; i++ )); do
-	randomString=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $(( RANDOM % 10 + 3 )) | head -n 1)
-	randomParty=$(sed -n "$(( RANDOM % $(wc -l < $1) + 1 ))p" $1)
+
+	randomString=""
+	for (( j=0; j<$length; j++ )); do
+		randomString=$randomString${characters:$(( RANDOM % ${#characters} )):1}
+	done
+
+	randomParty=$(shuf -n 1 $1)
+
 	echo "$randomString $randomParty" >> inputFile
 done
 
--- ASS2/processLogFile.sh
+++ ASS2/processLogFile.sh
@@ -5,13 +5,9 @@ if [ $# -ne 1 ]; then
 	exit 1
 fi
 
-# check if poll-log exists in current directory and can be read
 if [ ! -r $1 ]; then
 	echo "$1 does not exist in current directory or cannot be read"
 	exit 1
 fi
-
-# poll log is of format <voterName> <partyName>
-# from poll-log, count the number of votes for each party, if there is a duplicate name, keep only the first one, then in the pollerResaultsFile.sh, each line have the format: <partyName> <numVotes>
 
 cat $1 | awk '{print $2}' | sort | uniq -c | awk '{print $2, $1}' > pollerResaultsFile
