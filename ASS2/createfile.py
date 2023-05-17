2ead908711647d0f6286d6f9c8421d2300e6662d
byron <bg.anemogiannis@outlook.com>
2023-05-17 22:11:49 +0300

    fuck

--- ASS2/createfile.py
+++ ASS2/createfile.py
@@ -2,10 +2,17 @@
 
 import random
 
-def createFile():
+def createFile1():
 	file = open("inputFile.txt", "w")
 	for i in range(120):
 		file.write("voter_name" + str(i) + " " + "party_name" + str(random.randint(1, 5)) + "\n")
 	file.close()
+
+def createFile2():
+	file = open("politicalParties.txt", "w")
+	for i in range(10):
+		file.write("party_name" + str(i)+ "\n")
+	file.close()
     
-createFile()
+# createFile()
+createFile2()
