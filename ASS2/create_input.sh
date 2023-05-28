cce5c4b339e8bf258b8c3bae41c011add40873a5
byron <bg.anemogiannis@outlook.com>
2023-05-29 02:31:48 +0300

    SDBsbd

--- ASS2/create_input.sh
+++ ASS2/create_input.sh
@@ -9,7 +9,10 @@ if [ $# -ne 2 ]; then # check if the number of arguments is correct
 	exit 1
 fi
 
-touch inputFile # create the file
+if [ -f inputFile.txt ]; then
+	rm inputFile.txt
+fi
+touch inputFile.txt # create the file 
 
 for (( i=0; i<$2; i++ )); do
 	length=$(( RANDOM % 10 + 3 )) # random length of the name
@@ -19,9 +22,16 @@ for (( i=0; i<$2; i++ )); do
 		randomString=$randomString${characters:$(( RANDOM % ${#characters} )):1}
 	done
 
+	length=$(( RANDOM % 10 + 3 )) # random length of the name
+	
+	randomString2=""
+	for (( j=0; j<$length; j++ )); do
+		randomString2=$randomString2${characters:$(( RANDOM % ${#characters} )):1}
+	done
+
 	randomParty=$(shuf -n 1 $1) # choose a random party from the file
 
-	echo "$randomString $randomParty" >> inputFile # write the name and the party in the file
+	echo "$randomString $randomString2 $randomParty" >> inputFile.txt # write the name and the party in the file
 done
 
 
